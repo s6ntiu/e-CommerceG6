723a076ee5f6d6efb3fcf2ecf66f629b97ad773a
Leandronolosopor <salz.lean@gmail.com>
2026-05-16 19:23:41 -0300

    Creacion de Order.API y User.API

--- Products.API/Models/Product.cs
+++ Products.API/Models/Product.cs
@@ -2,5 +2,12 @@
 {
     public class Product
     {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public string Categoria { get; set; }
+        public DateTime FechaCreacion { get; set; }
     }
 }
