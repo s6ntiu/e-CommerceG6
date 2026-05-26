d39c471646fd5d7ca4927aecf6cd6e88cde86646
Santiago Ubeid <s6ntifn@gmail.com>
2026-05-26 17:22:06 -0300

    feature: endpoints registro, login y bloqueos, errores

--- User.API/Program.cs
+++ User.API/Program.cs
@@ -1,23 +1,36 @@
+using ECommerce.Shared.ExceptionHandlers;
+using User.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// --- REGISTRO DE SERVICIOS ---
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// Agrega el generador nativo de OpenAPI de .NET 9
 builder.Services.AddOpenApi();
+
+// Interceptores de excepciones globales de Leandro
+builder.Services.AddExceptionHandler<NotFoundExceptionHandler>();
+builder.Services.AddExceptionHandler<BusinessRuleExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+// Registro de tu servicio de usuarios
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// --- CONFIGURACIÓN DEL PIPELINE (MIDDLEWARES) ---
 if (app.Environment.IsDevelopment())
 {
+    // Mapea el endpoint nativo de OpenAPI (localhost:5028/openapi/v1.json)
     app.MapOpenApi();
 }
 
+// Activa el manejador global de errores de Leandro
+app.UseExceptionHandler();
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
