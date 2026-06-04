e8427d076ba4089dbe50297a8541b0ff4d381320
Leandronolosopor <salz.lean@gmail.com>
2026-06-03 22:55:14 -0300

    feat(orders): add Order models and DTOs

---
+++ Order.API/DTOs/OrderDTOs.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Order.API.DTOs;
+
+public class CreateOrderRequest
+{
+    [Required] public Guid UsuarioId { get; set; }
+    [Required, MinLength(1)] public List<OrderItemRequest> Items { get; set; } = new();
+}
+
+public class OrderItemRequest
+{
+    [Required] public Guid ProductoId { get; set; }
+    [Required, Range(1, int.MaxValue)] public int Cantidad { get; set; }
+}
+
+public class UpdateOrderStatusRequest
+{
+    [Required] public string Estado { get; set; } = string.Empty;
+}
